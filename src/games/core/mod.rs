b6b702655206a2e84ee4f77980b72fe151c8f2f7
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2022-06-17 12:00:51 -0400

    Changing phase from String to enum. Adding round and reset functionality.

--- src/games/core/mod.rs
+++ src/games/core/mod.rs
@@ -1,13 +1,50 @@
 
 pub struct GameCore {
-    phase: String
+    phase: Phase,
+    round: u16
+}
+
+#[derive(Debug, PartialEq)]
+enum Phase {
+    Boot,
+    Setup,
+    Play,
+    End,
 }
 
 impl GameCore {
+    /// GameCore constructor
     pub fn new() -> GameCore {
         GameCore {
-            phase: String::from("boot")
+            phase: Phase::Boot,
+            round: 0
         }
+    }
+
+    /// For progressing the phase of the game
+    pub fn next_phase(mut self) -> GameCore {
+        self.phase = match self.phase {
+            Phase::Boot => Phase::Setup,
+            Phase::Setup => Phase::Play,
+            Phase::Play => Phase::End,
+            Phase::End => Phase::End
+        };
+
+        self
+    }
+
+    /// For moving the game to the next round
+    pub fn next_round(mut self) -> GameCore {
+        self.round += 1;
+
+        self
+    }
+
+    /// For resetting the game to the initial state
+    pub fn reset(mut self) -> GameCore {
+        self = GameCore::new();
+
+        self
     }
 }
 
--- src/games/core/test.rs
+++ src/games/core/test.rs
@@ -1,7 +1,36 @@
 use super::*;
 
 #[test]
-fn phase() {
-    let game = GameCore::new();
-    assert_eq!(game.phase, "boot");
+fn keep_track_of_phase() {
+    let mut game = GameCore::new();
+    assert_eq!(game.phase, Phase::Boot);
+    game = game.next_phase();
+    assert_eq!(game.phase, Phase::Setup);
+    game = game.next_phase();
+    assert_eq!(game.phase, Phase::Play);
+    game = game.next_phase();
+    assert_eq!(game.phase, Phase::End);
+    game = game.next_phase();
+    assert_eq!(game.phase, Phase::End);
+}
+
+#[test]
+fn keep_track_of_round() {
+    let mut game = GameCore::new();
+    assert_eq!(game.round, 0);
+    game = game.next_round();
+    assert_eq!(game.round, 1);
+    game = game.next_round();
+    assert_eq!(game.round, 2);
+}
+
+#[test]
+fn reset_the_game() {
+    let mut game = GameCore::new();
+    game = game.next_phase().next_phase().next_round();
+    assert_eq!(game.phase, Phase::Play);
+    assert_eq!(game.round, 1);
+    game = game.reset();
+    assert_eq!(game.phase, Phase::Boot);
+    assert_eq!(game.round, 0);
 }
